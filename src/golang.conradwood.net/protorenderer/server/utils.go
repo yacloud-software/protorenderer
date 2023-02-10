461980feffeb2aec50551f02d497a8031e1e5e20
Conrad Wood <cnw@conradwood.net>
2023-02-10 20:09:13 +0000

    fix segfault
    Change-Id: I69595767ae379440b04ce381a9a74c3c7efe51f1

--- src/golang.conradwood.net/protorenderer/server/utils.go
+++ src/golang.conradwood.net/protorenderer/server/utils.go
@@ -3,16 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"golang.conradwood.net/go-easyops/auth"
 	"golang.conradwood.net/go-easyops/errors"
-	"golang.conradwood.net/go-easyops/rpc"
 )
 
 func NeedVersion(ctx context.Context) error {
 	if completeVersion != nil {
 		return nil
 	}
-	cs := rpc.CallStateFromContext(ctx)
-	s := fmt.Sprintf(fmt.Sprintf("%s.%s not available", cs.ServiceName, cs.MethodName))
+	svc := auth.UserIDString(auth.GetService(ctx))
+	uvc := auth.UserIDString(auth.GetUser(ctx))
+	s := fmt.Sprintf(fmt.Sprintf("%s.%s not available", uvc, svc))
 	fmt.Println(s)
 	return errors.Unavailable(ctx, s)
 }

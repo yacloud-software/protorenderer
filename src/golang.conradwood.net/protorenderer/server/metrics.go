929edf4cfeac7b20b95455cbab664fbd4b47b489
Conrad Wood <cnw@conradwood.net>
2023-08-07 08:12:50 +0100

    expose a failedGauge metric
    Change-Id: I4bf7eebc0b65f8568481edaf5150b98b2acf0d55

---
+++ src/golang.conradwood.net/protorenderer/server/metrics.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"golang.conradwood.net/go-easyops/prometheus"
+	"time"
+)
+
+var (
+	failedGauge = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "protorenderer_failed_protos",
+			Help: "V=1 UNIT=none DESC=number of proto files failed to compile",
+		},
+	)
+)
+
+func start_metrics() {
+	prometheus.MustRegister(failedGauge)
+	go start_metrics_loop()
+}
+func start_metrics_loop() {
+	t := time.Duration(3) * time.Second
+	for {
+		time.Sleep(t)
+		t = time.Duration(30) * time.Second
+		metrics_update()
+	}
+}
+func metrics_update() {
+	v := completeVersion
+	if v == nil {
+		return
+	}
+	failedGauge.Set(float64(len(v.failures.Failures())))
+}

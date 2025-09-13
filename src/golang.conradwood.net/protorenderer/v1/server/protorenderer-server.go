44141318f6ebe87404cc5cc2230f6ff785bf21f3
Conrad Wood <cnw@conradwood.net>
2025-09-13 13:18:10 +0100

    obs code

--- src/golang.conradwood.net/protorenderer/v1/server/protorenderer-server.go
+++ src/golang.conradwood.net/protorenderer/v1/server/protorenderer-server.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
+
 	srv1 "golang.conradwood.net/protorenderer/v1/srv"
+)
+
+var (
+	do_run = flag.Bool("obsolete_code", false, "if true run anyways")
 )
 
 func main() {
 	flag.Parse()
+	if !*do_run {
+		fmt.Printf("This is obsolete code and shall not run anymore.\n")
+		fmt.Printf("(also see -h)\n")
+		panic("obsolete code")
+	}
 	srv1.Start()
 }
